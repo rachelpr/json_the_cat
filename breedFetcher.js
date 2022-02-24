9bb48eb96d4459addcafed77316f7f7a2685620b
Rachel PR <rplourder@gmail.com>
2022-02-23 20:39:22 -0400

    Refactor some of the code, expor to index.js to run that instead to get breed description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,21 @@
 const request = require("request");
 
 
-const fetchDescription = function (breedName) {
-  breedName = process.argv[2]
+const fetchBreedDescription = function (breedName, callback) {
+
   url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, response, body) => {
-    if (error !== undefined) {
-      console.log(`Found an error: ${error}`)
-    }
 
     const data = JSON.parse(body);
 
     breed = data[0]
-
     if (breed) {
-      console.log(breed["description"])
+      callback(breed["description"])
     } else {
-      console.log(`Couldn't find that ${breedName} in the database`)
+      callback(`Couldn't find that ${breedName} in the database`)
     }
 
   });
 };
 
-fetchDescription();
+module.exports = { fetchBreedDescription }
